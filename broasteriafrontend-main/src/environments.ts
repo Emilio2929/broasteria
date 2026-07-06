4dfa602bdaffd46ae3475efe1ff951dd111240c3
Jerson <jersonmarin29@icloud.com>
2026-07-05 19:31:45 -0500

    Revert "Arquitectura unificada: Frontend y Backend en Azure con subdominios wildcard nip.io"
    This reverts commit c410db3b1d240f8a86fa79365925a6d4aa99a189.

--- broasteriafrontend-main/src/environments.ts
+++ broasteriafrontend-main/src/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
-  apiUrl: "https://api.172-184-131-149.nip.io",
-  wsUrl: "wss://api.172-184-131-149.nip.io"
+  production: false,
+  apiUrl: "http://localhost:8080",
+  wsUrl: "ws://localhost:8080"
 };
